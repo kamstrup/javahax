fb0930c552a9415f1e75f2c6bd9512490aa6d271
Mikkel Kamstrup Erlandsen <mikkel.kamstrup@gmail.com>
2014-02-06 22:48:43 +0100

    Add javadocs to BitField and BloomFilter

--- BitField.java
+++ BitField.java
@@ -5,31 +5,75 @@ import java.util.Arrays;
  * There is a default implementation backed by an 32 bit int array,
  * but the interface is simple enough to provide easy implementation
  * on top of memory mapped files or other structures.
+ * <p/>
+ * One notable difference between this interface and the standard
+ * {@link java.util.BitSet} is that a {@code BitField} can not expand
+ * dynamically.
  *
  * @author kamstrup
  */
 public interface BitField {
 
+    /**
+     * Set the bit at a given index to 1.
+     * <p/>
+     * Note the important difference between {@code BitField} and the standard
+     * Java {@link java.util.BitSet} is that that a bit field will not expand
+     * dynamically. It is a programming error to access indexes past the
+     * allocated bit field capacity.
+     *
+     * @param index the 0-based index of the bit to set
+     */
     public void set(int index);
 
+    /**
+     * Get the value of the bit at a given index.
+     * @param index the 0-based index to get
+     * @return {@code true} iff the bit is 1.
+     * @throws java.lang.ArrayIndexOutOfBoundsException if the index is out of
+     *                                                  bounds
+     */
     public boolean get(int index);
 
+    /**
+     * Set the bit at a given index to 0.
+     * @param index the 0-based index of the bit to set
+     */
     public void clear(int index);
 
+    /**
+     * Clear the entire bit set, setting all bits to 0.
+     */
     public void clear();
 
+    /**
+     * Return the number of bits available in this bit field.
+     * @return the number of bits available in this bit field.
+     */
     public int capacity();
 
-    public static BitField create(int capacity) {
-        return new BitFieldImpl(capacity);
+    /**
+     * Create a new {@code BitField} with <i>at least</i> {@code minCapacity}
+     * bits available. Implementations are free to allocate more than the
+     * requested capacity. It can typically be rounded up to the nearest
+     * 32- or 64-bit boundary.
+     *
+     * @param minCapacity The minimum capacity of the bit field
+     * @return a new bit field that can hold at least {@code minCapacity} bits
+     */
+    public static BitField create(int minCapacity) {
+        return new BitFieldImpl(minCapacity);
     }
 
+    /**
+     * Private impl class of BitField based on 32 bit ints.
+     */
     static class BitFieldImpl implements BitField {
 
         private final int[] values;
 
-        public BitFieldImpl(int capacity) {
-            values = new int[valueIndex(capacity) + 1];
+        public BitFieldImpl(int minCapacity) {
+            values = new int[valueIndex(minCapacity) + 1];
         }
 
         @Override
--- BloomFilter.java
+++ BloomFilter.java
@@ -1,16 +1,33 @@
 import java.util.*;
 
 /**
+ * High performance implementation of a Bloom Filter.
+ * <p/>
+ * The Bloom filter implementation provided by this class is about as fast
+ * as you get without implementing everything in native code or hardware.
+ * <p/>
+ * The default implementations of this interface guarantee that no operation
+ * allocates or frees memory. This mean that they guarantee zero GC overhead
+ * and are suitable for real-time systems.
+ *
  * @author kamstrup
  */
 public abstract class BloomFilter<T> {
 
     private static final float LN2 = (float) Math.log(2);
 
+    /**
+     * A function that returns a 32 bit hash code for an object of type {@code T}.
+     * @param <T> the type of objects this hash function applies to.
+     */
     public interface HashFunc<T> {
         public int hash(T val);
     }
 
+    /**
+     * A {@link HashFunc} that simply uses the default Java {@link #hashCode()}
+     * of the object.
+     */
     public static final HashFunc<Object> JAVA_HASH = new HashFunc<Object>() {
         @Override
         public int hash(Object val) {
@@ -18,14 +35,55 @@ public abstract class BloomFilter<T> {
         }
     };
 
+    /**
+     * Add a value to the Bloom filter.
+     * <p/>
+     * This operation has running time O(1).
+     *
+     * @param val the value to add
+     */
     public abstract void add (T val);
 
+    /**
+     * Check if an item matches the filter. Note that Bloom filter matching
+     * bears the risk of false positives.
+     * <p/>
+     * This operation has running time O(1).
+     * <p/>
+     * You can can get the false-positive probability by calling FIXME.
+     *
+     * @param val the value to check for.
+     * @return {@code true} if the item maybe is in the filter. {@code false}
+     *         implies that the item <i>definitely not</i> is in the filter.
+     */
     public abstract boolean maybeContains(T val);
 
+    /**
+     * Remove all items from the filter.
+     * <p/>
+     * This operation has running time O(m).
+     */
     public abstract void clear();
 
+    /**
+     * The number of times the {@link #add} method was called on this filter.
+     * Will be reset when {@link #clear} is called.
+     */
     public abstract int size();
 
+    /**
+     * Create a new Bloom filter.
+     * <p/>
+     * If both {@code capacity} and {@code bitsPerValue} are powers of 2
+     * this factory function will create a faster Bloom filter.
+     *
+     * @param capacity the number of values the filter should be able to store
+     * @param bitsPerValue number of bits to use per value. 10 is suitable for
+     *                     most applications.
+     * @param func the {@link BloomFilter.HashFunc} to use. Fx. {@link #JAVA_HASH}
+     * @param <T> the type of values that will be added to the filter
+     * @return a new filter
+     */
     public static <T> BloomFilter<T> create(int capacity, int bitsPerValue, HashFunc<? super T> func) {
         int k = (int)(LN2 * bitsPerValue);
         int m = capacity * bitsPerValue;
@@ -55,6 +113,9 @@ public abstract class BloomFilter<T> {
         return (int)h;
     }
 
+    /**
+     * Private impl class that can provide a Bloom filter for any (m,k).
+     */
     private static class DynamicBloomFilter<T> extends BloomFilter<T> {
 
         protected final BitField bits;
@@ -100,6 +161,7 @@ public abstract class BloomFilter<T> {
         @Override
         public void clear() {
             bits.clear();
+            count = 0;
         }
 
         @Override
@@ -114,8 +176,7 @@ public abstract class BloomFilter<T> {
 
     /**
      * If m is a power of two we can optimize the truncation of the hash value
-     * from being a modulo with the BitSet capacity, to simply masking with m-1.
-     * @param <T>
+     * from being a modulo with m, to simply masking with m-1.
      */
     private static class POTBloomFilter<T> extends DynamicBloomFilter<T> {
 
